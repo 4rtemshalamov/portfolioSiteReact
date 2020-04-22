3f9aa8ccd50abb8ebe9d57b7cfb566ad646d9aa1
Artem Shalamov <iart1756@gmail.com>
2020-04-22 21:21:54 +0500

    Updates

---
+++ precache-manifest.f00797af76d5b108e8ef7a9b58f4e54a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134cecaa43cd20d535d9c7d25e54fe1b",
+    "url": "/portfolioSiteReact/index.html"
+  },
+  {
+    "revision": "e30cf126b169d60f08fd",
+    "url": "/portfolioSiteReact/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "5e06225c34e83b23c98d",
+    "url": "/portfolioSiteReact/static/css/main.6810d3c6.chunk.css"
+  },
+  {
+    "revision": "e30cf126b169d60f08fd",
+    "url": "/portfolioSiteReact/static/js/2.56fce931.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolioSiteReact/static/js/2.56fce931.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e06225c34e83b23c98d",
+    "url": "/portfolioSiteReact/static/js/main.226d60cc.chunk.js"
+  },
+  {
+    "revision": "97f92be1579bad3d40a5",
+    "url": "/portfolioSiteReact/static/js/runtime-main.5a746b80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/artemWebDevep/myPortfolioReact/precache-manifest.f7164785179a871bbc7f6e1563636810.js"
+  "/portfolioSiteReact/precache-manifest.f00797af76d5b108e8ef7a9b58f4e54a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/artemWebDevep/myPortfolioReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolioSiteReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
